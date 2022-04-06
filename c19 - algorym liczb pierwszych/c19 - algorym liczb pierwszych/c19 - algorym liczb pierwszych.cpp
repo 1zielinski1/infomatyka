c3d31cc96143241dbb6ce1de0faaeeb1077683ee
Wiktor Zielinski <zielinski.of@gmail.com>
2022-04-06 10:46:05 +0200

    Updated reade

---
+++ c19 - algorym liczb pierwszych/c19 - algorym liczb pierwszych/c19 - algorym liczb pierwszych.cpp
@@ -0,0 +1,32 @@
+﻿// c19 - algorym liczb pierwszych.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int lp, p, n, d;
+    bool tf;
+    cout << "ile chcesz wyswietlic liczb pierwszych: ";
+    cin >> n;
+    lp = 0;
+    p = 2;
+    while (lp < n) {
+        tf = true;
+        for (d = 2; d < p; d++)
+            if (p % d == 0)
+            {
+                tf = false;
+                break;
+            }
+        if (tf)
+        {
+            cout << p << " ";
+            lp++;
+        }
+        p++;
+    }
+    cout << endl;
+    return 0;
+}
---
+++ c20 - NWD i NWW/c20 - NWD/c20 - NWD.cpp
@@ -0,0 +1,29 @@
+﻿// c20 - NWD.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int iloczyn;
+    cout << "Podaj dwie liczby: " << endl;
+    cin >> a >> b;
+    iloczyn = a * b;
+    while (a!=b){
+        if (a < b)
+        {
+            b = b - a;
+        }
+        else
+        {
+            a = a - b;
+        }
+        
+    }
+    cout << "NWD: " << a << endl;;
+    cout << "NWW: " << iloczyn/a;
+
+}
+    
